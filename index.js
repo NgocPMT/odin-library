dd64d162db3cccaf13a3514e5bf47226b4a0719a
NgocPMT <149455563+NgocPMT@users.noreply.github.com>
2025-04-25 11:44:06 +0700

    add the function for new book feature and its modal
    for creating new book

--- index.js
+++ index.js
@@ -42,8 +42,41 @@ bookContainer.innerHTML = books;
 
 const newBookButton = document.querySelector(".new-book-button");
 const addBookModal = document.querySelector("#add-book-modal");
-const modalAddBookButton = document.querySelector(".modal-add-button");
 const modalCloseButton = document.querySelector(".modal-close-button");
 
 newBookButton.addEventListener("click", () => addBookModal.showModal());
 modalCloseButton.addEventListener("click", () => addBookModal.close());
+
+const modalAddBookButton = document.querySelector(".modal-add-button");
+const bookTitleEL = document.querySelector("#title");
+const bookAuthorEL = document.querySelector("#author");
+const bookPagesEl = document.querySelector("#pages");
+const bookStatusEL = document.querySelector("#read");
+
+modalAddBookButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const bookTitle = bookTitleEL.value;
+  const bookAuthor = bookAuthorEL.value;
+  const bookPages = bookPagesEl.value;
+  const bookStatus = bookStatusEL.checked;
+
+  addBookToLibrary(
+    crypto.randomUUID(),
+    bookTitle,
+    bookAuthor,
+    bookPages,
+    bookStatus
+  );
+
+  const newBook = myLibrary[myLibrary.length - 1];
+  bookContainer.innerHTML += `
+    <div class="book-card">
+      <h3 class="book-title">${newBook.title}</h3>
+      <p class="book-author">${newBook.author}</p>
+      <p class="book-pages">${newBook.pages} pages</p>
+      <p class="book-status">${newBook.read ? "read" : "not read yet"}<p/>
+    </div>
+    `;
+
+  addBookModal.close();
+});
